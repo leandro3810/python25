34a98de0c1abe130d8dd30349295d3b82b600435
Leandro ribeiro gomes <ledielsong9@gmail.com>
2024-08-05 22:29:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    /// <summary>
+    /// Represents the entry point of the program.
+    /// </summary>
+    /// internal class Program
+    {
+        /// <summary>
+        /// The entry point of the program.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        /// static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");(let's see if this works),command-line arguments.
+        }
+    }
+}
