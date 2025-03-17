b68d35f46a5abf3ea77d75bdb47b86f1e57de52e
Leandro ribeiro gomes <ledielsong9@gmail.com>
2025-03-17 19:28:49 -0300

    Create _Deprecation_Warning.py

---
+++ _Deprecation_Warning.py
@@ -0,0 +1,7 @@
+class SetuptoolsDeprecationWarning(Warning):
+    """
+    Base class for warning deprecations in ``setu
+
+    This class is not derived from ``DeprecationW
+    visible by default.
+    """
