03110b53e6bc6699ca1cb438a11de70f74af6554
Leandro ribeiro gomes <ledielsong9@gmail.com>
2025-03-23 17:07:03 -0300

    Update Biblioteca Html5/_tokenizador.py
    self . tokenQueue . anexar (
                    {
                        "tipo" : tokenTypes [ "Caracteres" ],
                        "dados" : f"</ { self . temporaryBuffer } " ,
                    }
                )
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- Biblioteca Html5/_tokenizador.py
+++ Biblioteca Html5/_tokenizador.py
@@ -604,8 +604,12 @@ class HTMLTokenizer(object):
         elif data in asciiLetters:
             self.temporaryBuffer += data
         else:
-            self.tokenQueue.append({"type": tokenTypes["Characters"],
-                                    "data": "</" + self.temporaryBuffer})
+            self.tokenQueue.append(
+                {
+                    "type": tokenTypes["Characters"],
+                    "data": f"</{self.temporaryBuffer}",
+                }
+            )
             self.stream.unget(data)
             self.state = self.scriptDataState
         return True
